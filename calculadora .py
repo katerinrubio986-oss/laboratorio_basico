75fe2899a21fdc18b28aa58a59d57dacf445a451
Sara Katherine <@katerinrubio986>
2025-10-17 13:55:01 -0500

    Versión inicial del laboratorio básico

---
+++ calculadora .py
@@ -0,0 +1,7 @@
+# calculadora
+a = int ( input ( " Ingrese el primer numero: "))
+b = int ( input ( " Ingrese el segunfo numero: "))
+print ( " suma: ", a+b)
+print ( "resta: ", a-b)
+print ( "Multiplicacion: ", a*b)
+print ( "Division: ", a/b)
---
+++ par_o_impar .py
@@ -0,0 +1,5 @@
+nume = ( int ( input ("Ingrese un numero: ")))
+if nume % 2 == 0: 
+    print ("El numero esw par")
+else: 
+    print ( "El numero es impar" )
